baaf970a464cbef800bb2164953b633d5f3c93b1
hwms <seishin@gmail.com>
2026-07-13 05:43:30 +0200

    fiemap: handle sparse holes and clamp extents to EOF
    Handle leading, inner and trailing sparse holes without misreporting unchanged files as modified. Clamp FIEMAP tail extents to logical EOF so dedupe requests do not cross file size. Add regression coverage for sparse mappings and EOF clamping.

--- tests.c
+++ tests.c
@@ -1,4 +1,7 @@
 #include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
 #include <sys/stat.h>
 
 #include "minunit.h"
@@ -24,6 +27,20 @@
 
 unsigned int blocksize = DEFAULT_BLOCKSIZE;
 static char *exec_path;
+
+static struct fiemap *alloc_test_fiemap(unsigned int count, uint64_t file_size)
+{
+	struct fiemap *fiemap;
+
+	fiemap = calloc(1, sizeof(*fiemap) +
+			count * sizeof(struct fiemap_extent));
+	abort_on(!fiemap);
+
+	fiemap->fm_mapped_extents = count;
+	fiemap->fm_extent_count = count;
+	fiemap->fm_length = file_size;
+	return fiemap;
+}
 
 MU_TEST(test_is_block_zeroed) {
 	blocksize = 100;
@@ -92,10 +109,92 @@ MU_TEST(test_is_file_renamed) {
 	mu_check(is_file_renamed(exec_path, new_path) == false);
 }
 
+MU_TEST(test_get_extent_sparse_holes) {
+	struct fiemap *fiemap = alloc_test_fiemap(2, 64 * 1024);
+	struct fiemap_extent *extent;
+	unsigned int index;
+
+	fiemap->fm_extents[0].fe_logical = 4 * 1024;
+	fiemap->fm_extents[0].fe_length = 4 * 1024;
+	fiemap->fm_extents[1].fe_logical = 16 * 1024;
+	fiemap->fm_extents[1].fe_length = 4 * 1024;
+	fiemap->fm_extents[1].fe_flags = FIEMAP_EXTENT_LAST;
+
+	/* Leading hole. */
+	extent = get_extent(fiemap, 0, &index);
+	mu_check(index == 0);
+	mu_check(extent->fe_logical == 0);
+	mu_check(extent->fe_length == 4 * 1024);
+	mu_check(extent->fe_flags & FIEMAP_EXTENT_UNWRITTEN);
+	mu_check(!(extent->fe_flags & FIEMAP_EXTENT_LAST));
+
+	/* First mapped extent. */
+	extent = get_extent(fiemap, 4 * 1024, &index);
+	mu_check(index == 0);
+	mu_check(extent == &fiemap->fm_extents[0]);
+
+	/* Inner hole. */
+	extent = get_extent(fiemap, 8 * 1024, &index);
+	mu_check(index == 1);
+	mu_check(extent->fe_logical == 8 * 1024);
+	mu_check(extent->fe_length == 8 * 1024);
+	mu_check(extent->fe_flags & FIEMAP_EXTENT_UNWRITTEN);
+
+	/* Trailing hole. */
+	extent = get_extent(fiemap, 20 * 1024, &index);
+	mu_check(index == 2);
+	mu_check(extent->fe_logical == 20 * 1024);
+	mu_check(extent->fe_length == 44 * 1024);
+	mu_check(extent->fe_flags & FIEMAP_EXTENT_UNWRITTEN);
+	mu_check(extent->fe_flags & FIEMAP_EXTENT_LAST);
+
+	mu_check(get_extent(fiemap, 64 * 1024, NULL) == NULL);
+	free(fiemap);
+}
+
+MU_TEST(test_get_extent_fully_sparse_file) {
+	struct fiemap *fiemap = alloc_test_fiemap(0, 32 * 1024);
+	struct fiemap_extent *extent;
+
+	extent = get_extent(fiemap, 0, NULL);
+	mu_check(extent != NULL);
+	mu_check(extent->fe_logical == 0);
+	mu_check(extent->fe_length == 32 * 1024);
+	mu_check(extent->fe_flags & FIEMAP_EXTENT_UNWRITTEN);
+	mu_check(extent->fe_flags & FIEMAP_EXTENT_LAST);
+
+	free(fiemap);
+}
+
+MU_TEST(test_clamp_fiemap_extents_to_eof) {
+	struct fiemap *fiemap = alloc_test_fiemap(3, 10 * 1024);
+
+	fiemap->fm_extents[0].fe_logical = 0;
+	fiemap->fm_extents[0].fe_length = 4 * 1024;
+	fiemap->fm_extents[1].fe_logical = 8 * 1024;
+	fiemap->fm_extents[1].fe_length = 4 * 1024;
+	fiemap->fm_extents[2].fe_logical = 12 * 1024;
+	fiemap->fm_extents[2].fe_length = 4 * 1024;
+
+	clamp_fiemap_extents_to_eof(fiemap, 10 * 1024);
+
+	mu_check(fiemap->fm_length == 10 * 1024);
+	mu_check(fiemap->fm_mapped_extents == 2);
+	mu_check(fiemap->fm_extents[0].fe_length == 4 * 1024);
+	mu_check(fiemap->fm_extents[1].fe_logical == 8 * 1024);
+	mu_check(fiemap->fm_extents[1].fe_length == 2 * 1024);
+	mu_check(fiemap->fm_extents[1].fe_flags & FIEMAP_EXTENT_LAST);
+
+	free(fiemap);
+}
+
 MU_TEST_SUITE(test_suite) {
 	MU_RUN_TEST(test_is_block_zeroed);
 	MU_RUN_TEST(test_block_len);
 	MU_RUN_TEST(test_is_file_renamed);
+	MU_RUN_TEST(test_get_extent_sparse_holes);
+	MU_RUN_TEST(test_get_extent_fully_sparse_file);
+	MU_RUN_TEST(test_clamp_fiemap_extents_to_eof);
 }
 
 int main(int argc [[maybe_unused]], char *argv[]) {
